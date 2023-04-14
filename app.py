12bbd177d18a609dc6729509a11e2a89d88a2e74
mamadayozov <130375302+mamadayozov@users.noreply.github.com>
2023-04-14 20:00:20 +0500

    creat

---
+++ app.py
@@ -0,0 +1,25 @@
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+print(thislist[2])
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+thislist[2]=("grip")
+print (thislist)
+
+h=["pizza","boor"]
+
+thislist.append("mooze")
+thislist.extend(h)
+print(thislist)
+for b in thislist:
+    print(b)
+
+
+
+
+
+
+
+
+
